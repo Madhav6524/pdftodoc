806490a819b69e0dce6420a0975bb079ebf1a33f
Madhav6524 <madhav.20ff@gmail.com>
2026-07-06 22:34:46 +0530

    Update app.py

--- pdftodoc copy/app.py
+++ pdftodoc copy/app.py
@@ -1763,14 +1763,6 @@ def pdf_debug():
     return jsonify(result)
 
 
-from flask import Flask, request, send_file, jsonify, render_template
-
-app = Flask(
-    __name__,
-    static_folder="static",
-    template_folder="templates",
-    static_url_path="/static"
-)
 
 @app.route("/")
 def index():
